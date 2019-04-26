1e7c5ac6fe99ca6489564db078f64a9509353f6c
Matthias Mohr <m.mohr@uni-muenster.de>
2019-04-26 15:47:38 +0200

    Report an error for non-temporal reductions.

--- src/processes/reduce.js
+++ src/processes/reduce.js
@@ -5,6 +5,14 @@ module.exports = class reduce extends Process {
 	async execute(node, context) {
 		var dc = node.getData("data");
 		var dimension = node.getArgument("dimension");
+		var temporalDimension = dc.getDimension(dimension);
+		if (temporalDimension.type !== 'temporal') {
+			throw new Errors.ProcessArgumentInvalid({
+				process: this.schema.id,
+				argument: 'dimension',
+				reason: 'GEE can only reduce the temporal dimension at the moment.'
+			});
+		}
 		var callback = node.getArgument("reducer");
 		var resultNode = await callback.execute({
 			data: dc
