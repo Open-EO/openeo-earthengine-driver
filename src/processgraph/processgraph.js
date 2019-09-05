ea101f76910a2f859aa6ed53765a4fbb06b92897
Matthias Mohr <m.mohr@uni-muenster.de>
2019-09-05 15:11:15 +0200

    Optimized service processing time.

--- src/processgraph/processgraph.js
+++ src/processgraph/processgraph.js
@@ -1,12 +1,14 @@
 const { ProcessGraph } = require('@openeo/js-commons');
 const GeeProcessGraphNode = require('./node');
 const Errors = require('../errors');
+const Utils = require('../utils');
 
 module.exports = class GeeProcessGraph extends ProcessGraph {
 
 	constructor(jsonProcessGraph, context) {
 		super(jsonProcessGraph, context.server().processes());
 		this.context = context;
+		this.loadCollectionRect = null;
 	}
 
 	getContext() {
@@ -14,6 +16,16 @@ module.exports = class GeeProcessGraph extends ProcessGraph {
 	}
 
 	createNodeInstance(json, id, parent) {
+		// Optimization for web services to only load the extent of the data that is needed if no spatial extent is defined by the load_collection process
+		if (this.loadCollectionRect && json.process_id === 'load_collection' && Utils.isObject(json.arguments) && !json.arguments.spatial_extent) {
+			// ToDo: If an extent exists, use the intersecting area between tile and user-selected bounding box to further improve runtime.
+			json.arguments.spatial_extent = {
+				west: this.loadCollectionRect[0],
+				south: this.loadCollectionRect[1],
+				east: this.loadCollectionRect[2],
+				north: this.loadCollectionRect[3]
+			};
+		}
 		return new GeeProcessGraphNode(json, id, parent);
 	}
 
@@ -38,6 +50,10 @@ module.exports = class GeeProcessGraph extends ProcessGraph {
 	isReducer() {
 		var process = this.getParentProcess();
 		return (process && process.schema['gee:reducer'] === true);
+	}
+
+	optimizeLoadCollectionRect(rect) {
+		this.loadCollectionRect = rect;
 	}
 
 	addError(error) {
