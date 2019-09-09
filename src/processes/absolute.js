397e366d587a3031b64b06116fb36217313966ba
claxn <clax.n@gmx.at>
2019-09-09 03:12:22 +0200

    update of apply process

--- src/processes/absolute.js
+++ src/processes/absolute.js
@@ -3,8 +3,8 @@ const Commons = require('../processgraph/commons');
 
 module.exports = class absolute extends Process {
 
-    async execute(node, context) {
-        return Commons.applyInCallback(node, image => image.abs());
+    geeProcess(a){
+        return a.abs();
     }
 
 };
--- src/processes/apply.js
+++ src/processes/apply.js
@@ -5,8 +5,36 @@ module.exports = class apply extends Process {
     async execute(node, context) {
         var dc = node.getData("data");
         var callback = node.getArgument("process");
-        var resultNode = await callback.execute({x: dc});
+        var process = callback.getProcess(callback.getResultNode());
+        if (typeof process.geeProcess !== 'function') {
+            throw new Errors.ProcessArgumentInvalid({
+                process: this.schema.id,
+                argument: 'process',
+                reason: 'The specified process is invalid.'
+            });
+        }
+        var resultNode = this.simpleApply(dc, process.geeProcess(), process.id);
         dc = resultNode.getResult();
+        return dc;
+    }
+
+    //TODO
+    /*
+    complexApply(dc, applyFunc, applyName=null) {
+        if (!dc.isImageCollection() && !dc.isImage()) {
+            throw new Error("Calculating " + applyName + " not supported for given data type.");
+        }
+
+        dc.imageCollection(data => data.map(applyFunc));
+        return dc;
+    }*/
+
+    simpleApply(dc, applyFunc, applyName=null) {
+        if (!dc.isImageCollection() && !dc.isImage()) {
+            throw new Error("Calculating " + applyName + " not supported for given data type.");
+        }
+
+        dc.imageCollection(data => data.map(applyFunc));
         return dc;
     }
 
