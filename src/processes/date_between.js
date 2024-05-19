efedc80290ca477867733d5321a2cbaa428aa428
Matthias Mohr <webmaster@mamo-net.de>
2024-05-19 17:06:29 +0200

    Fix date processes

--- src/processes/date_between.js
+++ src/processes/date_between.js
@@ -1,12 +1,17 @@
 import GeeProcess from '../processgraph/process.js';
+import If from './if.js';
 
-export default class date_shift extends GeeProcess {
+export default class date_between extends GeeProcess {
 
   executeSync(node) {
-    const date = node.getArgumentAsDateEE('date');
-    const value = node.getArgumentAsNumberEE('value');
-    const unit = node.getArgumentAsStringEE('unit');
-    return date.advance(value, unit);
+    // Convert to numerical timestamps in milliseconds
+    const x = node.getArgumentAsDateEE('x').millis();
+    const min = node.getArgumentAsDateEE('min').millis();
+    const max = node.getArgumentAsDateEE('max').millis();
+    // Check if the max values are included or excluded
+    const exclude_max = node.getArgumentAsNumberEE('exclude_max', false);
+    const upper = If.process(node.ee, exclude_max, x.lt(max), x.lte(max));
+    return min.lte(x).and(upper);
   }
 
 }
--- src/processes/date_difference.js
+++ src/processes/date_difference.js
@@ -1,17 +1,12 @@
 import GeeProcess from '../processgraph/process.js';
-import If from './if.js';
 
-export default class date_shift extends GeeProcess {
+export default class date_difference extends GeeProcess {
 
   executeSync(node) {
-    // Convert to numerical timestamps in milliseconds
-    const x = node.getArgumentAsDateEE('x').millis();
-    const min = node.getArgumentAsDateEE('min').millis();
-    const max = node.getArgumentAsDateEE('max').millis();
-    // Check if the max values are included or excluded
-    const exclude_max = node.getArgumentAsNumberEE('exclude_max', false);
-    const upper = If.process(node.ee, exclude_max, x.lt(max), x.lte(max));
-    return min.lte(x).and(upper);
+    const date = node.getArgumentAsDateEE('date');
+    const value = node.getArgumentAsNumberEE('value');
+    const unit = node.getArgumentStringEE('unit');
+    return date.difference(value, unit);
   }
 
 }
