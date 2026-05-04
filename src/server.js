f7a1ba30e14114e255c9f906a10ee1f3814477a0
Matthias Mohr <m.mohr@moregeo.it>
2026-05-04 12:51:12 +0200

    Fix lint

--- src/server.js
+++ src/server.js
@@ -104,7 +104,7 @@ class Server {
 		app.use(this.errorHandler.bind(this));
 	}
 
-	errorHandler(err, req, res, next) {
+	errorHandler(err, req, res, next) { // eslint-disable-line no-unused-vars
 		if (global.server.serverContext.debug) {
 			if (err.originalError) {
 				console.error(err.originalError);
