ba2cd1d8962023fd14fcb5e79e020ea65c7b89b3
Matthias Mohr <m.mohr@moregeo.it>
2026-07-01 12:03:13 +0200

    Add pm2 config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	apps: [
+		{
+			name: "openeo-earthengine-server",
+			script: "src/server.js",
+			autorestart: true,
+			cron_restart: "0 0 * * *",
+			max_memory_restart: "500M",
+			exp_backoff_restart_delay: 100
+		}
+	]
+};
